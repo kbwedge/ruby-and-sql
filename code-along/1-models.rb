f84cd3f4ed58fbb8ee1b8754782513cf1a92afa1
kbwedge <kevin.wedge@kellogg.northwestern.edu>
2022-02-01 16:20:45 +0000

    Lab 1 complete

--- code-along/1-models.rb
+++ code-along/1-models.rb
@@ -10,11 +10,40 @@ Company.destroy_all
 
 # 1a. check out the schema file
 # 1b. check out the model file
+puts "There are now #{Company.all.count} companies."
+
 
 # 2. create new companies
+values = {name: "Apple, Inc.", url: "https://apple.com", city: "Cupertino", state: "CA"}
 
+apple = Company.new(values)
+apple.save
+puts "There are now #{Company.all.count} companies."
+
+values = {name: "Amazon, Inc.", url: "https://amazon.com", city: "Seattle", state: "IL"}
+
+amazon = Company.new(values)
+amazon.save
+puts "There are now #{Company.all.count} companies."
 # 3. query companies table
+puts Company.all.inspect 
+# pulls all records and attributes of company
+
 
 # 4. read column values from row
 
+CA_company = Company.where({state: "CA"})[0]
+puts CA_company.url
+
 # 5. update attribute value
+CA_company.slogan = "Think different."
+CA_company.save
+
+new_company = Company.new
+new_company.name = "Tesla, Inc."
+new_company.url = "https://tesla.com"
+new_company.city = "Palo Alto"
+new_company.state = "CA"
+new_company.save
+
+puts "There are now #{Company.all.count} companies."
--- labs/1-models.rb
+++ labs/1-models.rb
@@ -9,11 +9,48 @@ Contact.destroy_all
 # **************************
 
 # 1a. check out the schema file
+
 # 1b. check out the model file
 
 # 2. create 1-2 new contacts for each company (they can be made up)
+apple = Company.where({name: "Apple, Inc."})[0]
+amazon = Company.where({name: "Amazon, Inc."})[0]
+tesla = Company.where({name: "Tesla, Inc."})[0]
+
+new_contact = Contact.new
+new_contact.first_name = "Andy"
+new_contact.last_name = "Jassy"
+new_contact.company_id = amazon.id
+new_contact.email = "andy@amazon.com"
+new_contact.save
+
+new_contact = Contact.new
+new_contact.first_name = "Craig"
+new_contact.last_name = "Federighi"
+new_contact.company_id = apple.id
+new_contact.email = "craig@apple.com"
+new_contact.save
+
+new_contact = Contact.new
+new_contact.first_name = "Elon"
+new_contact.last_name = "Musk"
+new_contact.company_id = tesla.id
+new_contact.email = "elon@tesla.com"
+new_contact.save
+
+new_contact = Contact.new
+new_contact.first_name = "Tim"
+new_contact.last_name = "Cook"
+new_contact.company_id = apple.id
+new_contact.email = "tim@amazon.com"
+new_contact.save
 
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
+puts "Contacts: #{Contact.all.count}"
+
+for i in Contact.all
+    puts "#{i.first_name} #{i.last_name} - #{i.email}"
+end
 
 # ---------------------------------
 # Contacts: 4
